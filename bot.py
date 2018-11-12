12b5d8988cd1319d07590949b9959a364c4f27dc
Álvaro Garrido <alvarospunk@hotmail.com>
2018-11-12 17:10:56 +0100

    fix redundancies

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-
 def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
@@ -11,17 +10,14 @@ def start(bot, update):
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
-echo_handler = MessageHandler(Filters.text, echo)
+def answer(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="No contesto a tonterías")
 
 updater = Updater('462567247:AAEs76XLVZZeTesKJBShttI-XQASUic8yVU')
 
-
-from telegram.ext import MessageHandler, Filters
-echo_handler = MessageHandler(Filters.text, echo)
-
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(echo_handler)
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
 updater.start_polling()
 updater.idle()
