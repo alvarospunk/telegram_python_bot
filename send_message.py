24164c388013677cd341c3081a067359321a96fb
Álvaro Garrido <alvarospunk@hotmail.com>
2018-11-12 22:19:35 +0100

    added send_message.py

--- send_message.py
+++ send_message.py
@@ -5,7 +5,8 @@ id_killo = 66428972
 id_alvarospunk = 11644706
 id_arubeto = 256150411
 id_luli = 172812686
-id_javi = 12831239
+id_javi = 1283123
+id_hola = 12381238
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
